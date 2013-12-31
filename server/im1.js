bc38889de4f2d7a3ad0c496824e7c7fc90b882c2
Whit Schorn <ws@bit.ly>
2013-12-30 21:57:42 -0600

    client server separation

---
+++ server/im1.js
@@ -0,0 +1,8 @@
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+
+    
+    //api.add_files(['index.js', '../../packages.json'], 'server');
+  });
+}
