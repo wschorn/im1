9a0250db39bdcfe33c7d714c9b8e183fb92a42c1
Whit Schorn <ws@bit.ly>
2013-12-30 22:08:13 -0600

    actually calling the bitly api from meteor server-side. this is probably an abomination of synchronization, but I'm happy.

--- server/im1.js
+++ server/im1.js
@@ -1,8 +1,15 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    console.log("I am the server. The server is me.");
+    var Bitly = Meteor.require("bitly-oauth");
+	var b = new Bitly("wschornmeteor", "throwaway1");
+	//console.log(b);
+	b.link.clicks({ link: "http://bit.ly/Ki1L7Y" }, function(err, result){
+  // do stuff
+  	console.log(result);
+});
 
-    
     //api.add_files(['index.js', '../../packages.json'], 'server');
   });
 }
